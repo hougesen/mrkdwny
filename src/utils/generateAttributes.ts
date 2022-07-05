a4eb899c7da8bddafc8cf994737efff7b3e7444e
Mads Hougesen <madshougesen@gmail.com>
2022-07-05 07:01:10 +0200

    feat: attributes for tags

---
+++ src/utils/generateAttributes.ts
@@ -0,0 +1,35 @@
+import { IMrkdwnyOptions } from '..';
+
+export function generateAttributes(options: IMrkdwnyOptions = {}) {
+    const attributes = {
+        p: '',
+        a: '',
+        img: '',
+        h1: '',
+        h2: '',
+        h3: '',
+        h4: '',
+        h5: '',
+        h6: '',
+    };
+
+    const optionKeys = Object.keys(options ?? {});
+
+    if (optionKeys.length) {
+        for (const key of optionKeys) {
+            // TODO: fix this type
+            // @ts-expect-error ???
+            const keyAttributes = Object.keys(options[key] ?? {});
+
+            if (keyAttributes.length) {
+                for (const attribute of keyAttributes) {
+                    // TODO: fix this type
+                    // @ts-expect-error ???
+                    attributes[key] += ` ${attribute}="${options[key][attribute]}"`;
+                }
+            }
+        }
+    }
+
+    return attributes;
+}
--- src/utils/parseLink.ts
+++ src/utils/parseLink.ts
@@ -1,16 +1,37 @@
+import { IMrkdwnyOptions } from '..';
 import { LINKRegex } from './regex';
 
 // TODO: handle title [text](https://l.ink "title")
-export function generateATag(line: string): string {
+export function generateATag(line: string, options?: IMrkdwnyOptions['a']): string {
     const linkParts = line?.replace('[', '')?.replace(')', '');
 
     const [text, href, _] = linkParts.split('](');
 
-    return `<a href="${href}">${text}</a>`;
+    let attributes = '';
+
+    if (options?.class?.length && typeof options?.class === 'string') {
+        attributes += ` class="${options?.class}"`;
+    }
+
+    if (options?.style?.length && typeof options?.style === 'string') {
+        attributes += ` style="${options?.style}"`;
+    }
+
+    return `<a href="${href}"${attributes}>${text}</a>`;
 }
 
-export function parseLink(line: string): string {
-    let element = '<p>';
+export function parseLink(line: string, options?: IMrkdwnyOptions): string {
+    let attributes = '';
+
+    if (options?.p?.class?.length && typeof options?.p?.class === 'string') {
+        attributes += ` class="${options?.p?.class}"`;
+    }
+
+    if (options?.p?.style?.length && typeof options?.p?.style === 'string') {
+        attributes += ` style="${options?.p?.style}"`;
+    }
+
+    let element = `<p${attributes}>`;
 
     while (line.length) {
         const matched = line.match(LINKRegex);
@@ -18,7 +39,7 @@ export function parseLink(line: string): string {
         if (matched?.length) {
             const [before, after] = line.split(matched[0]);
 
-            const linkElement = generateATag(matched[0]);
+            const linkElement = generateATag(matched[0], options?.a);
 
             element += before;
             element += linkElement;
