c99db167293c0689bd88df4090f1d05a368c51a5
Mads Hougesen <madshougesen@gmail.com>
2022-07-06 17:04:07 +0200

    refactor: simplified generateStrongTag

--- src/utils/generateStrongTag.ts
+++ src/utils/generateStrongTag.ts
@@ -1,11 +1,3 @@
 export function generateStrongTag(line: string, type: string, attributes = ''): string {
-    if (type === '_') {
-        return line?.replace('__', `<strong${attributes}>`)?.replace('__', '</strong$>');
-    }
-
-    if (type === '*') {
-        return line?.replace('**', `<strong${attributes}>`)?.replace('**', '</strong>');
-    }
-
-    return '';
+    return line?.replace(type, `<strong${attributes}>`)?.replace(type, '</strong>');
 }
