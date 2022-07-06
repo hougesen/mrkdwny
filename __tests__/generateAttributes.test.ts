7c6db9ef267ff8bcef6f4bf6005467bc7ea21060
Mads Hougesen <madshougesen@gmail.com>
2022-07-06 17:45:21 +0200

    test: added units for element attributes

--- __tests__/generateAttributes.test.ts
+++ __tests__/generateAttributes.test.ts
@@ -13,4 +13,59 @@ describe('generateAttributes', () => {
             })
         ).toHaveProperty('h1', ' class="heading bold" style="text-align:center;"');
     });
+
+    it('h2 element', () => {
+        expect(
+            generateAttributes({
+                h2: {
+                    class: 'heading bold',
+                    style: 'text-align:center;',
+                },
+            })
+        ).toHaveProperty('h2', ' class="heading bold" style="text-align:center;"');
+    });
+
+    it('h3 element', () => {
+        expect(
+            generateAttributes({
+                h3: {
+                    class: 'heading bold',
+                    style: 'text-align:center;',
+                },
+            })
+        ).toHaveProperty('h3', ' class="heading bold" style="text-align:center;"');
+    });
+
+    it('h2 element', () => {
+        expect(
+            generateAttributes({
+                h4: {
+                    class: 'heading bold',
+                    style: 'text-align:center;',
+                },
+            })
+        ).toHaveProperty('h4', ' class="heading bold" style="text-align:center;"');
+    });
+
+    it('h2 element', () => {
+        expect(
+            generateAttributes({
+                h5: {
+                    class: 'heading bold',
+                    style: 'text-align:center;',
+                },
+            })
+        ).toHaveProperty('h5', ' class="heading bold" style="text-align:center;"');
+    });
+
+    it('h2 element', () => {
+        expect(
+            generateAttributes({
+                h6: {
+                    class: 'heading bold',
+                    style: 'text-align:center;',
+                },
+            })
+        ).toHaveProperty('h6', ' class="heading bold" style="text-align:center;"');
+    });
 });
--- __tests__/parseDocument.test.ts
+++ __tests__/parseDocument.test.ts
@@ -1,12 +1,105 @@
 import { describe, expect, it } from 'vitest';
+import { IMrkdwnyOptions } from '../src';
 
 import { parseDocument } from '../src/utils/parseDocument';
 import mockResults from './mock-data/mockResults';
+
+const dummyOptions: IMrkdwnyOptions = {
+    elements: {
+        p: {
+            class: 'text',
+            style: 'text-align: center',
+        },
+        a: {
+            target: '_blank',
+            rel: 'noopener noreferrer',
+            referrerpolicy: 'true',
+        },
+        strong: {
+            class: 'bold',
+        },
+        em: {
+            class: 'italic',
+        },
+        img: {
+            loading: 'lazy',
+        },
+        h1: {
+            class: 'heading1',
+            style: 'text-align: center',
+        },
+        h2: {
+            class: 'heading2',
+            style: 'text-align: center',
+        },
+        h3: {
+            class: 'heading3',
+            style: 'text-align: center',
+        },
+        h4: {
+            class: 'heading4',
+            style: 'text-align: center',
+        },
+        h5: {
+            class: 'heading5',
+            style: 'text-align: center',
+        },
+        h6: {
+            class: 'heading6',
+            style: 'text-align: center',
+        },
+    },
+};
 
 describe('parseDocument.ts', () => {
     it('validate mock results', () => {
         for (const { markdown, html } of mockResults) {
             expect(parseDocument(markdown)).toEqual(html);
         }
+    });
+
+    it('validate that heading options work', () => {
+        for (let i = 1; i <= 6; i += 1) {
+            expect(parseDocument(` heading ${i}`.padStart(` heading ${i}`.length + i, '#'), dummyOptions)).toEqual(
+                `<h${i} class="heading${i}" style="text-align: center">heading ${i}</h${i}>`
+            );
+        }
+    });
+
+    it('validate that paragraph options work', () => {
+        expect(parseDocument('this is a paragraph', dummyOptions)).toEqual(
+            '<p class="text" style="text-align: center">this is a paragraph</p>'
+        );
+    });
+
+    it('validate that img options work', () => {
+        expect(parseDocument('![alt](src)', dummyOptions)).toEqual(
+            '<p class="text" style="text-align: center"><img src="src" alt="alt" loading="lazy" /></p>'
+        );
+    });
+
+    it('validate that a options work', () => {
+        expect(parseDocument('[text](href)', dummyOptions)).toEqual(
+            '<p class="text" style="text-align: center"><a href="href" target="_blank" rel="noopener noreferrer" referrerpolicy="true">text</a></p>'
+        );
+    });
+
+    it('validate that em options work', () => {
+        expect(parseDocument('*italic text*', dummyOptions)).toEqual(
+            '<p class="text" style="text-align: center"><em class="italic">italic text</em></p>'
+        );
+
+        expect(parseDocument('_italic text_', dummyOptions)).toEqual(
+            '<p class="text" style="text-align: center"><em class="italic">italic text</em></p>'
+        );
+    });
+
+    it('validate that strong options work', () => {
+        expect(parseDocument('**bold text**', dummyOptions)).toEqual(
+            '<p class="text" style="text-align: center"><strong class="bold">bold text</strong></p>'
+        );
+        expect(parseDocument('__bold text__', dummyOptions)).toEqual(
+            '<p class="text" style="text-align: center"><strong class="bold">bold text</strong></p>'
+        );
     });
 });
